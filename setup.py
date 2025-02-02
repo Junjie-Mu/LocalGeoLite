821ec29c048262510a5a4a0f3de43373b5d8e7c3
Junjie-Mu <junjie.mu@mail.polimi.it>
2025-02-02 18:58:19 +0100

    Configuration test

--- setup.py
+++ setup.py
@@ -10,9 +10,12 @@ setup(
     url="https://github.com/Junjie-Mu/LocalGeoLite",
     packages=find_packages(),
     install_requires=[
-        "torch",
+        #!!IMPORTANT: Torch need to be manually installed according to the CUDA version.
+        #"torch"
         "transformers",
-        "unsloth",
+        "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git",
+        "numpy",
+        "jupyter lab",
     ],
     extras_require={
         'test': [
@@ -25,7 +28,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     entry_points={
         'console_scripts': [
             'localgeo=LocalGeoLite.cli:main',
